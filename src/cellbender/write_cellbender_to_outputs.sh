ec440bc9c87c9c80eb916456ce96919b2c6ce67c
Adam Catching <adamcatching@gmail.com>
2024-08-26 14:22:41 -0400

    Writes individual bash scripts for an array job

--- src/cellbender/write_cellbender_to_outputs.sh
+++ src/cellbender/write_cellbender_to_outputs.sh
@@ -4,6 +4,15 @@
 # NIA-CARD/Data Tecnica
 # 2024-08-22
 
+"""
+This is the working file for creating a swarm job that processes Cellranger->Cellbender.
+Currently the main issue is that the checkpoint file only writes to the directory from 
+which the command was executed. Workaround is using cellbender_array.sh, as a Sbatch 
+array job. When Cellbender is updated such that the checkpoint file can be written 
+to another folder this script will work. 
+"""
+
+
 # Find the sample locations where CellRanger has been run
 input_file_base=/data/CARD_singlecell/Brain_atlas/SN_Multiome/
 input_file_locs=$(echo /data/CARD_singlecell/Brain_atlas/SN_Multiome/*/Multiome/*/outs/)
@@ -12,7 +21,7 @@ output_file_base=/data/CARD_singlecell/SN_atlas/cellbender
 
 # Rewrite the file with your changes
 rm $output_file_base/cellbender.swarm
-touch $output_file_base/cellbender.swarm
+#touch $output_file_base/cellbender.swarm
 
 # Turn list of Cellranger output folders into a list of locations
 input_array=(`echo ${input_file_locs}`);
@@ -30,19 +39,30 @@ PMI Ethnicity Race Brain_bank Short Diagnosis
 do
     # Create directories of the samples (ONLY RUN ONCE)
     # mkdir $output_file_base"/"$Sample_ID"-ARC"
-    # Write the swarm commands
+
+    # Inititiate the script file
+    touch $output_file_base"/"$Sample_ID"-ARC/cellbender.sh"
+    # Write out the Cellranger command to this bash script
     echo "module load cellbender; module load CUDA; cellbender remove-background --input "\
     $input_file_base"batch"$Use_batch"/Multiome/"$Sample_ID"-ARC/outs/raw_feature_bc_matrix.h5"\
     " --output "$output_file_base"/"$Sample_ID"-ARC/cellbender_gex_counts.h5 --checkpoint "\
-    $output_file_base"/"$Sample_ID"-ARC/ckpt.tar.gz" >>\
-    $output_file_base"/cellbender.swarm"
+    $output_file_base"/"$Sample_ID"-ARC/ckpt.tar.gz --fpr 0 --cuda" >>\
+    $output_file_base"/"$Sample_ID"-ARC/cellbender.sh"
+
+    # Holdover when all files were written to one swarm file
+    #$output_file_base"/cellbender.swarm"
 done < <(tail -n +2 /data/CARD_singlecell/SN_atlas/data/SNsamples.csv)
 
 # Repeat the cellbender command one last time (fix this later)
+
+# Creat the directory of the file sample
+# mkdir $output_file_base"/"$Sample_ID"-ARC"
+# Initiate the file
+touch $output_file_base"/"$Sample_ID"-ARC/cellbender.sh"
+# Write out the Cellranger command to the bash script
 echo "module load cellbender; module load CUDA; cellbender remove-background --input "\
 $input_file_base"batch"$Use_batch"/Multiome/"$Sample_ID"-ARC/outs/raw_feature_bc_matrix.h5"\
 " --output "$output_file_base"/"$Sample_ID"-ARC/cellbender_gex_counts.h5 --checkpoint "\
-$output_file_base"/"$Sample_ID"-ARC/ckpt.tar.gz" >>\
+$output_file_base"/"$Sample_ID"-ARC/ckpt.tar.gz --fpr 0 --cuda " >>\
 $output_file_base"/cellbender.swarm"
-
-#--partition=gpu --localcores=2 --localmem=64 --gres=gpu:v100x:1 "
+#$output_file_base"/"$Sample_ID"-ARC/cellbender.sh"
