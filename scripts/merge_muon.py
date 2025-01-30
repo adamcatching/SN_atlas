44ec41887e9efbc723e8386332a8d5b8103c5146
adamcatching <adamcatching@gmail.com>
2025-01-30 15:11:21 -0500

    Fixed bug that created multiome object from strings

--- scripts/merge_muon.py
+++ scripts/merge_muon.py
@@ -1,5 +1,6 @@
 import muon as mu
+import scanpy as sc
 
-mdata = mu.MuData({"RNA": snakemake.input.merged_rna_anndata, "ATAC": snakemake.input.merged_atac_anndata})
+mdata = mu.MuData({"RNA": sc.read_h5ad(snakemake.input.merged_rna_anndata), "ATAC": sc.read_h5ad(snakemake.input.merged_atac_anndata)})
 
-mdata.write(snakefile.output.merged_multiome)
+mdata.write_h5mu(snakefile.output.merged_multiome)
